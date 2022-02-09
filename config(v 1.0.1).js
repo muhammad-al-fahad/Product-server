7fc4338e9719d74e3d9c6d974ba95a5e02b1aeba
Muhammad Al Fahad <85945538+muhammad-al-fahad@users.noreply.github.com>
2022-02-09 19:07:18 +0500

    Add files via upload

---
+++ config(v 1.0.1).js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/product',
+    'facebook': {
+        clientId: '1396834357414619',
+        clientSecret: '52d5cfd570d97e70360bc0b4d06a7ec9'
+    }
+}
